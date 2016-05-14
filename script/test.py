a558acca4353cac11b1c5c20f47a8697f9357727
Novarizark <dennis-1991@qq.com>
2016-05-14 20:26:43 +0800

    commit py

--- script/test.py
+++ script/test.py
@@ -4,7 +4,7 @@ from datetime import datetime
 
 
 daily_flow_head = ['date', 'flow']
-daily_flow = pd.Series.from_csv('../data/daily-flow-full-record/uniq/daily-flow-A', parse_dates=True)
+daily_flow = pd.Series.from_csv('../data/daily-flow-full-record/uniq/daily-flow-D', parse_dates=True)
 #print daily_flow
 plt.plot(daily_flow,'k-')
 plt.show()
